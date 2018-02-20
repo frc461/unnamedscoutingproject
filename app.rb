a335dcfad10e7f5ac2eb79ad28fb868acfb54464
LS-4366-461 <luke.silver.ls@gmail.com>
2018-02-19 19:25:44 -0500

    Computer is righter than you

--- app.rb
+++ app.rb
@@ -1,27 +1,15 @@
 require 'sinatra/base'
 
 class ScoutingProject < Sinatra::Base
-get '/' do
-  erb :index
-end
-
-helpers do
-  def partial(template, *args)
-    options = args.extract_options!
-    options.merge!(:layout => false)
-    if collection = options.delete(:collection) then
-      collection.inject([]) do |buffer, member|
-        buffer << erb(template, options.merge(
-                                  :layout => false,
-                                  :locals => {template.to_sym => member}
-                                )
-                     )
-      end.join("\n")
-    else
-      erb(template, options)
+    get '/' do
+        erb :index
     end
-  end
-end
+
+    helpers do
+        def partial(page, options={})
+            erb page, options.merge!(:layout => false)
+        end
+    end
 end
 
 
