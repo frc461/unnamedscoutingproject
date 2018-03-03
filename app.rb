9f3cd64018da3c76ecb013e41ff27d47de5b3174
LS-4366-461 <luke.silver.ls@gmail.com>
2018-03-03 13:17:46 -0500

    It fix

--- app.rb
+++ app.rb
@@ -10,16 +10,16 @@ class ScoutingProject < Sinatra::Base
             erb page, options.merge!(:layout => false)
         end
     end
-    
+
     post "/form" do
-      payloads = [
-        JSON.parse( params['payload1'] ),
-        JSON.parse( params['payload2'] ),
-        JSON.parse( params['payload3'] } 
-      ]
-      client = Mongo::Client.new{[ '127.0.0.127017' ], :database => 'unnamedScoutingProject' )
-      db = client.database
-      collection = client[:data]
-      collection.insert_many(payloads) 
+        payloads = [
+            JSON.parse( params['payload1'] ),
+            JSON.parse( params['payload2'] ),
+            JSON.parse( params['payload3'] ) 
+        ]
+        client = Mongo::Client.new([ '127.0.0.127017' ], :database => 'unnamedScoutingProject') 
+                                   db = client.database
+                                   collection = client[:data]
+                                   collection.insert_many(payloads) 
     end
 end
