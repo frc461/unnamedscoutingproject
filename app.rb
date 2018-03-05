4f3f0c0b4064b0cbb7e164a73ab04384a6890dc1
LS-4366-461 <luke.silver.ls@gmail.com>
2018-03-05 18:21:05 -0500

    Fix app.rb

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require 'sinatra/base'
+require 'mongo'
 
 class ScoutingProject < Sinatra::Base
     configure do 
-      client = Mongo::Client.new{[ '127.0.0.127017' ], :database => 'unnamedScoutingProject' )
-      db = client.database
-      set :mongo_db, db[:unnamedScoutingProject]
+        client = Mongo::Client.new [ '127.0.0.127017' ], :database => 'unnamedScoutingProject' 
+                                   db = client.database
+                                   set :mongo_db, db[:unnamedScoutingProject]
     end
 
     get '/' do
@@ -18,26 +19,17 @@ class ScoutingProject < Sinatra::Base
     end
 
     post "/form" do
-      payloads = [
-        JSON.parse( params['payload1'] ),
-        JSON.parse( params['payload2'] ),
-        JSON.parse( params['payload3'] } 
-      ]
-      collection = client[:data]
-      collection.insert_many(payloads) 
         payloads = [
             JSON.parse( params['payload1'] ),
             JSON.parse( params['payload2'] ),
             JSON.parse( params['payload3'] ) 
         ]
-        client = Mongo::Client.new([ '127.0.0.127017' ], :database => 'unnamedScoutingProject') 
-                                   db = client.database
-                                   collection = client[:data]
-                                   collection.insert_many(payloads) 
+        collection = client[:data]
+        collection.insert_many(payloads) 
     end
 
     get '/raw/:team' do
-       hash = client[:data].find({teamid: params['team']}).first
-       hash.to_json
+        hash = client[:data].find({teamid: params['team']}).first
+        hash.to_json
     end
 end
