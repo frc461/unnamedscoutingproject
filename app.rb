364b8ad0931544a12a2b2900a1fdc65ec69c1c5b
Brad Thompson <bkt@brad-thompson.com>
2018-03-21 22:46:40 +0000

    modified form submission method and some other stuff

--- app.rb
+++ app.rb
@@ -31,26 +31,30 @@ class ScoutingProject < Sinatra::Base
   end
 
   post "/form" do
-    payloads = [
-      JSON.parse( params['payload1'] ),
-      JSON.parse( params['payload2'] ),
-      JSON.parse( params['payload3'] ) 
-    ]
-    settings.mongo_db.insert_many(payloads) 
+    3.times do |i|
+      obj = JSON.parse(params["payload#{i+1}"])
+      if obj == settings.mongo_db.find({matchnumber: obj[ 'matchnumber' ], teamid: obj[ 'teamid' ]}).first
+        settings.mongo_db.find_one_and_update({matchnumber: obj[ 'matchnumber' ], teamid: obj[ 'teamid' ]}, obj)
+      else
+        settings.mongo_db.insert_one(obj)
+      end
+    end
+  end
+
+  get '/all' do
+    settings.mongo_db.find.map{|e| e}.to_json
   end
 
   get '/raw' do
-      hash = settings.mongo_db.find({teamid: true })
-       hash.to_json
-    end
+    hash = settings.mongo_db.find({team: {'$exists' => true}})
+    hash.to_a.to_json
+  end
 
+  get '/raw/:team' do
+    settings.mongo_db.find({team: params['team']}).to_a.to_json
+  end
 
-    get '/raw/:team' do
-      hash = settings.mongo_db.find({teamid: params['team']}).first
-       hash.to_json
-    end
-
-    get '/scoutmaster' do
+  get '/scoutmaster' do
     unless data = settings.mongo_db.find({futurematch: true}).first
       settings.mongo_db.insert_one({futurematch: true, R1: '', R2: '', R3: '', B1: '', B2: '', B3: '', MN: '', EV: ''})
       data = settings.mongo_db.find({futurematch: true}).first
@@ -63,16 +67,16 @@ class ScoutingProject < Sinatra::Base
     @B1 = data['B1']
     @B2 = data['B2']
     @B3 = data['B3']
-	erb :scoutmaster	
-    end
+    erb :scoutmaster	
+  end
 
-    get '/future' do
-      settings.mongo_db.find({futurematch: true}).first.to_json
-    end
+  get '/future' do
+    settings.mongo_db.find({futurematch: true}).first.to_json
+  end
 
-    post '/scoutmaster/submit' do
-      data = {R1: params['R1'], R2: params['R2'], R3: params['R3'], B1: params['B1'], B2: params['B2'], B3: params['B3'], MN: params['MN'], EV: params['EV']}
-      settings.mongo_db.find_one_and_update({futurematch: true}, {'$set' => data})
-      redirect '/scoutmaster'
-    end
+  post '/scoutmaster/submit' do
+    data = {R1: params['R1'], R2: params['R2'], R3: params['R3'], B1: params['B1'], B2: params['B2'], B3: params['B3'], MN: params['MN'], EV: params['EV']}
+    settings.mongo_db.find_one_and_update({futurematch: true}, {'$set' => data})
+    redirect '/scoutmaster'
+  end
 end
