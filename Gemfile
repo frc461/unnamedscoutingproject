5675e22bafff4dc319cb99b59a26b45903e39608
Brad Thompson <bkt@brad-thompson.com>
2018-03-26 23:06:10 +0000

    TBA stuff, updated match data updating

--- Gemfile
+++ Gemfile
@@ -4,4 +4,4 @@ gem 'sinatra'
 gem 'mongo', '2.4.1'
 gem 'faye'
 gem 'puma'
-
+gem 'httparty'
---
+++ lib/tba.rb
@@ -0,0 +1,51 @@
+require 'yaml'
+require 'httparty'
+require 'json'
+require 'yaml'
+
+class TheBlueAlliance
+  raw_config = File.read('./config.yml')
+  CONFIG = YAML.load(raw_config)
+  include HTTParty
+  base_uri 'https://www.thebluealliance.com/api/v3'
+
+  def initialize
+    @options = {headers: {'X-TBA-App-Id': 'frc461:usp:v0', 'X-TBA-Auth-Key': CONFIG['tba_api_key']}}
+    @cacheCalls = {}
+  end
+
+  def call endpoint, options={}
+    if @cacheCalls.has_key? endpoint
+      options.merge!({'If-Modified-Since': @cacheCalls[endpoint]['lastmodified']})
+    else
+      @cacheCalls[endpoint] ||= {}
+    end
+
+    puts @options.merge(options)
+
+    response = self.class.get(endpoint, @options.merge(options))
+    puts response.code
+    puts response.headers
+    case response.code
+    when 304
+      @cacheCalls[endpoint]['body']
+    when 200
+      @cacheCalls[endpoint]['lastmodified'] = response.headers['last-modified']
+      @cacheCalls[endpoint]['body'] = JSON.parse(response.body)
+    end
+  end
+
+  def events options={}
+    call '/events/2018', options
+  end
+  
+  def getevent key
+    endpoint = '/event/' + key
+    call endpoint 
+  end
+
+  def getmatches key
+    endpoint = '/event/' + key + '/matches'
+    call endpoint 
+  end
+end
